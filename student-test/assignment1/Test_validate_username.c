e8a8fddfb7eea3ad6a28d4566f7925d0517544a1
Mohammed Khalaf <mohamedkhalaf20172020@gmail.com>
2024-12-27 04:14:48 +0200

    modifications of Assignments 3 part 1

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -5,18 +5,21 @@
 #include "../../assignment-autotest/test/assignment1/username-from-conf-file.h"
 
 /**
-* This function should:
-*   1) Call the my_username() function in Test_assignment_validate.c to get your hard coded username.
-*   2) Obtain the value returned from function malloc_username_from_conf_file() in username-from-conf-file.h within
-*       the assignment autotest submodule at assignment-autotest/test/assignment1/
-*   3) Use unity assertion TEST_ASSERT_EQUAL_STRING_MESSAGE the two strings are equal.  See
-*       the [unity assertion reference](https://github.com/ThrowTheSwitch/Unity/blob/master/docs/UnityAssertionsReference.md)
-*/
+ * This function should:
+ *   1) Call the my_username() function in autotest-validate.c to get your hard coded username.
+ *   2) Obtain the value returned from function malloc_username_from_conf_file() in
+ * username-from-conf-file.h within the assignment autotest submodule at
+ * assignment-autotest/test/assignment1/ 3) Use unity assertion TEST_ASSERT_EQUAL_STRING_MESSAGE to
+ * verify the two strings are equal.
+ */
 void test_validate_my_username()
 {
-    /**
-     * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
-     * config file and my_username() functions are setup properly
-     */
-    TEST_ASSERT_EQUAL_STRING_MESSAGE(my_username(), malloc_username_from_conf_file(), "Wrong user name.");
+	// Step 1: Get the hardcoded username
+	const char *hardcoded_username = my_username();
+
+	// Step 2: Get the username from the config file
+	const char *conf_file_username = malloc_username_from_conf_file();
+
+	// Step 3: Compare the two usernames using the Unity assertion
+	TEST_ASSERT_EQUAL_STRING_MESSAGE(hardcoded_username, conf_file_username, "Mohammedkh97");
 }
